0c8731e782b2f8bdae515afc0b07f75885b87c54
Arjav Poudel <arjav.poudel@ukaea.uk>
2024-06-25 15:11:20 +0100

    modify to use specific_heat + surf_heat_flux as material props to perturb

--- dataset_generator.py
+++ dataset_generator.py
@@ -4,7 +4,6 @@ EXAMPLE: Run MOOSE in sequential then parallel mode with mooseherder
 
 '''
 
-import sys
 
 
 import torch
@@ -17,11 +16,11 @@ from mooseherder import (MooseHerd,
 import random
 
 
-def generate_values(shc_ground_truth=400, 
-                    thermal_c_ground_truth=10,
-                    p=0.7,
+def generate_values(shc_ground_truth=406.0, 
+                    heat_flux_ground_truth=500.0e3,
+                    p=0.57,
                     n_dims=2, 
-                    n_samples=50):
+                    n_samples=30):
     """
     generate_values: generate perturbed values using sobol sampling.\n 
     Args:
@@ -35,15 +34,14 @@ def generate_values(shc_ground_truth=400,
     sobol_samples = soboleng.draw(n_samples)
 
     shc_samples = (shc_ground_truth * (1 + (2 * sobol_samples[:, 0] - 1) * p)).tolist()
-    thermal_conductivity_samples = (thermal_c_ground_truth * (1 + (2 * sobol_samples[:, 1] - 1) * p)).tolist()
+    heat_flux_samples = (heat_flux_ground_truth * (1 + (2 * sobol_samples[:, 1] - 1) * p)).tolist()
 
     random.shuffle(shc_samples)
-    random.shuffle(thermal_conductivity_samples)
+    random.shuffle(heat_flux_samples)
 
-    return (shc_samples, thermal_conductivity_samples)
+    return (shc_samples, heat_flux_samples)
 
 
-INPUT = sys.argv[1]
 
 def main():
     """main: run moose once, sequential then parallel.
@@ -54,7 +52,7 @@ def main():
 
     config_path = Path.cwd() / 'moose-config.json'
     moose_config = MooseConfig().read_config(config_path)
-    moose_input = Path(Path.cwd(), 'ground_truth_sims/sim-workdir-1', INPUT)
+    moose_input = Path(Path.cwd(), 'ground_truth_thermal_sim.i')
 
     moose_modifier = InputModifier(moose_input,'#','')
     moose_runner = MooseRunner(moose_config)
@@ -72,46 +70,24 @@ def main():
     herd.set_num_para_sims(n_para=10)
 
      # Send all the output to the examples directory and clear out old output
-    dir_manager.set_base_dir(Path('mooseherder_examples/'))
+    dir_manager.set_base_dir(Path('dataset/'))
     dir_manager.clear_dirs()
     dir_manager.create_dirs()
 
     # Create variables to sweep in a list of dictionaries, 8 combinations possible.
     specific_heat = generate_values()[0]
-    thermal_conductivity = generate_values()[1]
+    surface_heat_flux = generate_values()[1]
     moose_vars = list([])
     for sh in specific_heat:
-        for tc in thermal_conductivity:
+        for hf in surface_heat_flux:
             # Needs to be list[list[dict]] - outer list is simulation iteration,
             # inner list is what is passed to each runner/inputmodifier
-            moose_vars.append([{'specific_heat':sh,'thermal_conductivity':tc}])
+            moose_vars.append([{'cuSpecHeat':sh,'surfHeatFlux':hf}])
 
     print('Herd sweep variables:')
     for vv in moose_vars:
         print(vv)
 
-    # print()
-    # print("-"*80)
-    # print('EXAMPLE: Run MOOSE once')
-    # print("-"*80)
-
-    # Single run saved in moose-workdir-1
-    # herd.run_once(0,moose_vars[0])
-
-    # print(f'Run time (once) = {herd.get_iter_time():.3f} seconds')
-    # print("-"*80)
-    # print()
-
-    # print("-"*80)
-    # print('EXAMPLE: Run MOOSE sequentially')
-    # print("-"*80)
-
-    # # Run all variable combinations (8) sequentially in moose-workdir-1
-    # herd.run_sequential(moose_vars)
-
-    # print(f'Run time (seq) = {herd.get_sweep_time():.3f} seconds')
-    # print("-"*80)
-    # print()
 
     # print("-"*80)
     print('EXAMPLE: Run MOOSE in parallel')
