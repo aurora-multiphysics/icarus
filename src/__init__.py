ccdfc5b0467e949e4d0f8f5dd3023e2840eb46e5
Arjav Poudel <arjav.poudel@ukaea.uk>
2024-07-22 15:02:43 +0100

    remove imports from template init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-icarus
+thermal_demo
 """
 
-from icarus.ThermalModelDataset import ThermalModelDataset
-from icarus.GeneratePerturbedSamples import GeneratePerturbedSamples
-from mooseherder.mooserunner import MooseRunner
-from mooseherder.gmshrunner import GmshRunner
-from mooseherder.exodusreader import ExodusReader
-from mooseherder.mooseherd import MooseHerd
-from mooseherder.directorymanager import DirectoryManager
-from mooseherder.sweepreader import SweepReader
-from mooseherder.simdata import SimData
-from mooseherder.simdata import SimReadConfig
-from mooseherder.mooseconfig import MooseConfig
+from .dataset_generator import GeneratePerturbedSamples
+from .dataloader import ThermalModelDataset
+
 
 
 __all__ = ["thermalmodeldataset",
-            "generateperturbedsamples",
-            "mooserunner",
-            "gmshrunner",
-            "exodusreader",
-            "mooseherd",
-            "directorymanager",
-            "sweepreader",
-            "simdata",
-            "mooseconfig"]
+            "generateperturbedsamples",]
