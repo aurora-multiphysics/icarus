f9b6cb3b5468a47fe2fcf1a8b60fe1a3c35a3a89
Arjav Poudel <arjav.poudel@ukaea.uk>
2024-07-23 15:05:44 +0100

    renamed class to SampleGenerator

--- src/thermal_demo/__init__.py
+++ src/thermal_demo/__init__.py
@@ -3,12 +3,12 @@
 thermal_demo
 """
 
-from thermal_demo.dataset_generator import GeneratePerturbedSamples
+from thermal_demo.sample_generator import SampleGenerator
 from thermal_demo.dataloader import ThermalModelDataset
 from thermal_demo.model import IcarusModel
 
 
 
 __all__ = ["thermalmodeldataset",
-            "generateperturbedsamples",
+            "samplegenerator",
             "icarusmodel"]
--- src/thermal_demo/sample_generator.py
+++ src/thermal_demo/sample_generator.py
@@ -18,7 +18,7 @@ warnings.filterwarnings("ignore", category=UserWarning, module="skopt.sampler.so
 N_SAMPLES = 100
 np.random.seed(123)
 
-class GeneratePerturbedSamples:
+class SampleGenerator:
     '''generates perturbed parameter samples. using quasirandom (sobol) sampling within stratified samples to ensure dataset balance and 
        and sufficient coverage of the continous sample space, as specified by the perturbation factor,  and the tolerance level, t. both hyperparamterters can be altered by user 
     '''
@@ -108,64 +108,4 @@ class GeneratePerturbedSamples:
         ax.set_ylim([self.heat_flux_base * (1-self.p_factor), self.heat_flux_base * (1+self.p_factor)])
         plt.title('Stratified Sobol Samples')
         plt.show()
-
-
-dataset = GeneratePerturbedSamples(n_samples=1000, p_factor=0.8, tolerance=0.015)
-
-
-def main():
-    """main: run moose once, sequential then parallel.
-    """
-    print("-"*80)
-    print('EXAMPLE: Herd Setup')
-    print("-"*80)
-
-    config_path = Path.cwd() / 'moose-config.json'
-    moose_config = MooseConfig().read_config(config_path)
-    moose_input = Path(Path.cwd(), 'ground_truth_thermal_sim.i')
-
-    moose_modifier = InputModifier(moose_input,'#','')
-    moose_runner = MooseRunner(moose_config)
-    moose_runner.set_run_opts(n_tasks = 1,
-                          n_threads = 4,
-                          redirect_out = True)
-
-    dir_manager = DirectoryManager(n_dirs=1)
-
-    # Start the herd and create working directories
-    herd = MooseHerd([moose_runner],[moose_modifier],dir_manager)
-
-    # Set the parallelisation options
-    herd.set_num_para_sims(n_para=10)
-
-     # Send all the output to the examples directory and clear out old output
-    dir_manager.set_base_dir(Path('dataset/'))
-    dir_manager.clear_dirs()
-    dir_manager.create_dirs()
-
-    # Create variables to sweep in a list of dictionaries
-    moose_vars = list([])
-    thermal_cond, surface_heat_flux = dataset.generate_stratified_samples()
-    assert len(thermal_cond) == len(surface_heat_flux), "Mismatched lengths in generated samples"
-    for tc,hf in zip(thermal_cond, surface_heat_flux):    
-            moose_vars.append([{'cuThermCond':tc,'surfHeatFlux':hf}])
-
-    print('Herd sweep variables:')
-    for vv in moose_vars:
-        print(vv)
-
-
-
-    print('EXAMPLE: Run MOOSE in parallel')
-    print("-"*80)
-
-    herd.run_para(moose_vars)
-
-    print(f'Run time (para) = {herd.get_sweep_time():.3f} seconds')
-    print("-"*80)
-    print()
-
-
-if __name__ == '__main__':
-    main()
 
