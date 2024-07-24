e2842d834413f4479e116bf07e0845251662b4dc
Arjav Poudel <arjav.poudel@ukaea.uk>
2024-07-24 15:07:19 +0100

    update to include thermal_dataset_loading options

--- src/icarus/__init__.py
+++ src/icarus/__init__.py
@@ -6,7 +6,7 @@ icarus
 from icarus.sample_generator import SampleGenerator
 from icarus.dataloader import ThermalModelDataset
 from icarus.model import IcarusModel
-
+from icarus.datasets import load_thermal_dataset
 
 
 __all__ = ["thermalmodeldataset",
