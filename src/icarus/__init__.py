e94eadc25ef19d9d520f6734c3badd31fcd030c2
Arjav Poudel <arjav.poudel@ukaea.uk>
2024-07-30 15:42:21 +0100

    make utils.py class based

--- src/icarus/__init__.py
+++ src/icarus/__init__.py
@@ -5,10 +5,12 @@ icarus
 
 from icarus.sample_generator import SampleGenerator
 from icarus.dataloader import ThermalModelDataset
+from icarus.utils import CreateThermalDataset
 from icarus.model import IcarusModel
-from icarus.datasets import load_thermal_dataset
+from icarus.datasets import load_2d_thermal_dataset, load_monoblock_thermal_dataset
 
 
 __all__ = ["thermalmodeldataset",
             "samplegenerator",
-            "icarusmodel"]
+            "icarusmodel",
+            "createthermaldataset"]
