83c1b8803eab73120772ae9d89d1e461999d7aa1
Arjav Poudel <arjav.poudel@ukaea.uk>
2024-08-23 14:32:21 +0100

    add classes to init.py

--- src/icarus/__init__.py
+++ src/icarus/__init__.py
@@ -3,14 +3,7 @@
 icarus
 """
 
-from icarus.sample_generator import SampleGenerator
+from icarus.sample_generator import CreateThermalDataset, SimulationParameters
 from icarus.dataloader import ThermalModelDataset
-from icarus.utils import CreateThermalDataset
-from icarus.model import IcarusModel
+from icarus.model import IcarusModel, NNParameters
 from icarus.datasets import load_2d_thermal_dataset, load_monoblock_thermal_dataset
-
-
-__all__ = ["thermalmodeldataset",
-            "samplegenerator",
-            "icarusmodel",
-            "createthermaldataset"]
